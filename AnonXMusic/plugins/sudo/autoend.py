fa543f40f6e77ffa6aa3685fc169d582e9d65023
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-07-26 01:23:48 +0300

    Update autoend.py

--- AnonXMusic/plugins/sudo/autoend.py
+++ AnonXMusic/plugins/sudo/autoend.py
@@ -8,17 +8,17 @@ from AnonXMusic.utils.database import autoend_off, autoend_on
 
 @app.on_message(filters.command("autoend") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "<b>Örnek :</b>\n\n/autoend [Enable | Disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "» Akışı Otomatik Sonlandırma Etkinleştirildi..\n\nAsistan, Kimse Dinlemediğinde Birkaç Dakika Sonra Otomatik Olarak Görüntülü Sohbetten Ayrılacak."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("» Akışı Otomatik Sonlandırma Devre Dışı Bırakıldı.")
     else:
         await message.reply_text(usage)
